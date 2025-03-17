0a99923c5f7c9b1de137f0dded392662d29cd453
mabanana <aldwin.yu@hotmail.com>
2025-03-17 02:42:55 -0400

    Fix window scaling

--- Main.gd
+++ Main.gd
@@ -6,15 +6,25 @@ var screen: Rect2
 var taskbar_height: int
 
 var character_scene: PackedScene
-
-@export var timer: Timer
+@export_category("Game Parameters")
+@export var screen_height: int
 @export var spawn_cd: float
+@export var x_padding: int
+
+@export_category("Child Nodes")
+@export var timer: Timer
+@export var floor_body: StaticBody2D
+@export var screen_area: Area2D
+@export var screen_area_rect: CollisionShape2D
+
 
 var char_name_list = ["goblin_archer", "halfling_assassin"]
 
 func _ready():
+	# TODO: Changing height in project settings does not scale game correctly
 	_setup_window()
-	$Area2D.body_exited.connect(_on_body_leave_screen)
+	screen_area.body_exited.connect(_on_body_leave_screen)
+	
 	character_scene = load("res://character_body.tscn")
 	timer.start(spawn_cd)
 	timer.timeout.connect(func():
@@ -23,7 +33,7 @@ func _ready():
 		)
 
 func  _setup_window():
-	_update_screen_size()
+	_update_screen_size(screen_height)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, true)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, true)
@@ -33,17 +43,23 @@ func  _setup_window():
 	# TODO: Setup mouse passthrough
 	_update_tb_position(0)
 
-func _update_screen_size() -> void:
+func _update_screen_size(screen_height) -> void:
 	# Example: If window size changes (e.g., screen resolution change)
+	
 	screen = DisplayServer.screen_get_usable_rect()
-	window_width = screen.size.x
-	window_height = get_viewport_rect().size.y
+	window_width = screen.size.x - x_padding
+	window_height = screen_height
+	print(window_width, ", ", window_height)
+	get_window().set_size(Vector2(window_height, window_width))
+	screen_area_rect.shape.set_size(Vector2(window_height, window_width))
 	DisplayServer.window_set_size(Vector2i(window_width, window_height))
+	floor_body.position = Vector2(window_width/2, window_height + 150)
+	print(floor_body.position)
 	_update_screen_position()
 
 func _update_screen_position() -> void:
 	var screen_y = screen.size.y + screen.position.y - taskbar_height - window_height
-	DisplayServer.window_set_position(Vector2i(0, screen_y))
+	DisplayServer.window_set_position(Vector2i(x_padding / 2, screen_y))
 
 func _update_tb_position(value: float = 0) -> void:
 	value = int(value * 10)
@@ -52,7 +68,7 @@ func _update_tb_position(value: float = 0) -> void:
 
 func _on_body_leave_screen(body: Node2D):
 	if body is CharacterBody2D:
-		var x = 0 if body.position.x > 0 else window_width
+		var x = 0 if body.team == 0 else window_width
 		body.position = Vector2i(x, window_height)
 
 func spawn_character(name, team = 0):
